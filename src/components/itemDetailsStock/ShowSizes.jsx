b047f29f7a4a9891bc2fdf91e0ae6f3a8a09241d
ValenOrduna <valenordu@hotmail.com>
2022-09-16 17:18:37 -0300

    Context Solucionado

--- src/components/itemDetailsStock/ShowSizes.jsx
+++ src/components/itemDetailsStock/ShowSizes.jsx
@@ -2,13 +2,18 @@ import { useEffect, useState } from "react"
 
 
 const ShowSizes = ({talles,stock,setStock,setCantidad,setEstadoAgregar}) => {
-    
     const [tallesDisponibles,setTallesDisponibles]=useState([])
     const [estadoStock,setEstadoStock]=useState(false)
 
+    const handleClick = (e) =>{
+        setStock(e.target.value)
+        setCantidad(0)
+        setEstadoAgregar(true)
+    }
+
     useEffect(()=>{
         const promesaStock=new Promise ((res,rej)=>{
-            let totalStock=0,totalTalle=[['ELIGE UN TALLE','']],tallesDePrendas=38
+            let totalStock=0,totalTalle=[],tallesDePrendas=38
             talles.map((talle)=>{
                 tallesDePrendas+=1
                 const cadaTalle=[tallesDePrendas,talle]
@@ -26,21 +31,15 @@ const ShowSizes = ({talles,stock,setStock,setCantidad,setEstadoAgregar}) => {
 
   return (
     <div>
-        <select className="p-2 bg-black text-white font-bold my-5 text-center" name="talles">
+        <div>
             {tallesDisponibles.map((talle)=>{
-                let estadoTalle=true
-                talle[1]===0 ? estadoTalle=false : estadoTalle=true
-                if(estadoTalle){
-                    return <option key={talle[0]}  onClick={(e)=>{
-                        setStock(e.target.value)
-                        setCantidad(0)
-                        setEstadoAgregar(true)
-                    }} value={talle[1]}>{talle[0]}</option>
+                if(talle[1]!==0){
+                    return <button className="p-2 mx-3 font-bold text-white bg-black" onClick={(e)=>handleClick(e)} key={talle[0]} value={talle[1]}>{talle[0]}</button>
                 }else{
-                    return <option key={talle[0]}  disabled value={talle[1]}>{talle[0]}</option>
+                    return <button className="p-2 mx-3 font-bold text-white bg-black opacity-30" key={talle[0]}  disabled value={talle[1]}>{talle[0]}</button>
                 }
             })}
-        </select>
+        </div>
     </div>
   )
 }
